ea37abb449115db026d6bc402e9b0bc0cc877084
Asiphe Hashe <AHashe@sars.gov.za>
2026-04-20 12:22:45 +0200

    Changed the greeting

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Alex Khumalo");
         System.out.println("How are you?");
-        System.out.println("Hello from Asiphe");
+        System.out.println("Hey from the other side");
 
     }
 }
