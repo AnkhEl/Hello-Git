d9164cdefd1dfdc28178eca4fd844b07acb4ca2e
Alex Khumalo <t.alex.khumalo@gmail.com>
2026-04-20 07:30:06 +0200

    Update greeting message in new-branch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("How are you doing Sir Alex!");
+        System.out.println("Hello from NEW branch - hope you're learning Git!");
     }
 }
