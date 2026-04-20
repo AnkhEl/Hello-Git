93eea8c18831bd22ab2775f6075d046d515e44fe
Alex Khumalo <t.alex.khumalo@gmail.com>
2026-04-20 11:39:49 +0200

    edited Akhona's message

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Alex");
-        System.out.println("Hello from Akhona");
+        System.out.println("Hello from Akhona, Alex edited!");
         System.out.println("Hello from Asiphe");
 
     }
