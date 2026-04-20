3679d42c1c642b868a036140625f17cba31c38ce
Alex Khumalo <t.alex.khumalo@gmail.com>
2026-04-20 07:45:13 +0200

    Added some changes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello from NEW branch - hope you're learning Git!");
+        System.out.println("Hello from NEW branch - hope you're learning Git and are excelling!");
     }
 }
