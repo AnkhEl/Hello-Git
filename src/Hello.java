400d3c40e1f359c4d544368364b0f56d84968afe
Asiphe Hashe <AHashe@sars.gov.za>
2026-04-20 12:12:31 +0200

    Added how are you message

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Alex Khumalo");
-        System.out.println("Hello from Akhona");
+        System.out.println("How are you?");
         System.out.println("Hello from Asiphe");
 
     }
